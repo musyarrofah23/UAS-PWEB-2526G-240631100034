b3498bd50ce1f729de59eac01879d02cc60e0b7a
musyarrofah23 <pribadisaya598@gmail.com>
2026-06-27 11:59:42 +0700

    Improve UI with CSS

---
+++ navbar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MoneyTrack</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container">
+
+        <a class="navbar-brand fw-bold" href="index.php">
+            💰 MoneyTrack
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#menu">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <div class="collapse navbar-collapse" id="menu">
+
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="tambah.php" class="nav-link">Tambah Data</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="daftar.php" class="nav-link">Daftar Data</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
+
+<div class="container mt-4">
